1b29b1a291c43816e15f09d9928650761754bf0b
Jvi <javierlopez25177152@gmail.com>
2026-05-01 21:27:24 -0600

    Add Music 3+3Coins and end screen

--- planetSimu/planetSimu.cpp
+++ planetSimu/planetSimu.cpp
@@ -1,36 +1,77 @@
-﻿#include <SFML/Graphics.hpp>
+﻿
+/*
+Citas
+
+Musica
+    Cancion de fondo
+        Fujii, S. (2014). Sweet Sweet Canyon [Song]. In Mario Kart 8 Original Soundtrack. Nintendo.
+
+Efecto de sonido
+    Sonido de moneda
+        Kondo, K. (1985). Coin sound effect [Sound effect]. In Super Mario Bros. Nintendo.
+*/
+
+
+
+
+
+
+#include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <cmath>
-#include <Windows.h>
+
 #include "Point.h"
 #include <thread>
 
 
 sf::SoundBuffer buffer;
 
-
+int coinsColected = 0;
 
 float ballSpeed = 1;
-bool coinStatus = true;
+bool coinStatus1 = true;
+bool coinStatus2 = true;
+bool coinStatus3 = true;
+bool coinStatus4 = true;
+bool coinStatus5 = true;
+bool coinStatus6 = true;
 
 
 const double k = 9 * std::pow(10, 9);
 
 const double carga = 1.602 * std::pow(10, -19);
 
-void hitBox(float x1, float y1, Point coint, sf::CircleShape& visCoint,sf::Sound sdEffect) {
+void hitBox(float x1, float y1, Point coint, sf::CircleShape& visCoint,sf::Sound sdEffect, bool& coinSt) {
 
 
 
     if ((x1 >= coint.getX() - 20) and (x1 <= coint.getX() + 20) and (y1 >= coint.getY() - 25) and (y1 <= coint.getY() + 50)) {
-        if (coinStatus==true) {
+        if (coinSt==true) {
 
             visCoint.setFillColor(sf::Color::Black);
             sdEffect.play();
             while (sdEffect.getStatus() == sf::Sound::Status::Playing)
                 sf::sleep(sf::milliseconds(1));
-            coinStatus = false;
-            
+            coinSt = false;
+            coinsColected += 1;
+        }
+    }
+
+
+}
+void hitBoxSmall(float x1, float y1, Point coint, sf::CircleShape& visCoint, sf::Sound sdEffect, bool& coinSt) {
+
+
+
+    if ((x1 >= coint.getX() - 15) and (x1 <= coint.getX() + 15) and (y1 >= coint.getY() - 20) and (y1 <= coint.getY() + 40)) {
+        if (coinSt == true) {
+
+            visCoint.setFillColor(sf::Color::Black);
+            sdEffect.play();
+            while (sdEffect.getStatus() == sf::Sound::Status::Playing)
+                sf::sleep(sf::milliseconds(1));
+            coinSt = false;
+            coinsColected += 1;
         }
     }
 
@@ -51,17 +92,51 @@ void playMusic() {
         sf::sleep(sf::milliseconds(100));
 }
 
-
+//Movable point
 Point pointTest(400.f, 700.f);
+//Static points
 Point pointTest2(900.f, 700.f);
 
-
+//Coins
 Point golden(1500.f, 600.f);
+Point golden2(1300.f, 300.f);
+Point golden3(800.f, 100.f);
+
+Point golden4(1100.f, 800.f);
+Point golden5(900.f, 900.f);
+Point golden6(700.f, 800.f);
 
 
 
 int main()
 {
+    //Font/text
+    sf::Font font;
+    font.openFromFile("arial.ttf");
+    sf::Text wasdInstructions(font);
+    wasdInstructions.setString("WASD for Movement");
+    wasdInstructions.setCharacterSize(20);           // in pixels
+    wasdInstructions.setFillColor(sf::Color::White);
+    wasdInstructions.setPosition({ 100.f, 70.f });
+    sf::Text resetInstr(font);
+    resetInstr.setString("Press R to reset ball");
+    resetInstr.setCharacterSize(20);           // in pixels
+    resetInstr.setFillColor(sf::Color::White);
+    resetInstr.setPosition({ 100.f, 100.f });
+
+    sf::Text numOfCoins(font);
+    numOfCoins.setString("Coins collected " + std::to_string(coinsColected));
+    numOfCoins.setCharacterSize(30);           // in pixels
+    numOfCoins.setFillColor(sf::Color::White);
+    numOfCoins.setPosition({ 1400.f, 80.f });
+
+    sf::Text WinMessage(font);
+    WinMessage.setString("You Win");
+    WinMessage.setCharacterSize(200);           // in pixels
+    WinMessage.setFillColor(sf::Color::White);
+    WinMessage.setPosition({ 550.f, 350.f });
+
+
     //Music
     std::thread musicThread(playMusic);
     musicThread.detach();
@@ -82,6 +157,13 @@ int main()
     sf::CircleShape circleRef2(5.f);
 
     sf::CircleShape goldenView(20.f);
+    sf::CircleShape goldenView2(20.f);
+    sf::CircleShape goldenView3(15.f);
+
+    sf::CircleShape goldenView4(20.f);
+    sf::CircleShape goldenView5(15.f);
+    sf::CircleShape goldenView6(20.f);
+
 
     // Customize the circle
     circle.setFillColor(sf::Color::White);
@@ -91,7 +173,7 @@ int main()
     //CircleRef
     circleRef.setFillColor(sf::Color::Red);
     circleRef.setOutlineColor(sf::Color::White);
-    circleRef.setOutlineThickness(1.f);
+    circleRef.setOutlineThickness(2.f);
     //circleRef.setOrigin({ 250.f, 300.f });
 
 
@@ -104,6 +186,27 @@ int main()
     goldenView.setFillColor(sf::Color::Yellow);
     goldenView.setPosition({ golden.getX(), golden.getY() });
 
+    goldenView2 = goldenView;
+    goldenView3 = goldenView;
+    goldenView4 = goldenView;
+    //goldenView5 = goldenView;
+    goldenView6 = goldenView;
+    /*
+    goldenView2.setOutlineColor(sf::Color::White);
+    goldenView2.setOutlineThickness(1.f);
+    goldenView2.setFillColor(sf::Color::Yellow);*/
+    goldenView2.setPosition({ golden2.getX(), golden2.getY() });
+    
+    goldenView3.setPosition({ golden3.getX(), golden3.getY() });
+
+    goldenView4.setPosition({ golden4.getX() - 10, golden4.getY() });
+
+    goldenView5.setOutlineColor(sf::Color::White);
+    goldenView5.setOutlineThickness(1.f);
+    goldenView5.setFillColor(sf::Color::Yellow);
+    goldenView5.setPosition({ golden5.getX() - 10, golden5.getY() });
+
+    goldenView6.setPosition({ golden6.getX() - 10, golden6.getY() });
 
 
     float initX = 100;
@@ -189,7 +292,17 @@ int main()
         initX = initX + forceX;
         initY = initY + forceY;
 
-        hitBox(initX, initY, golden, goldenView,soundCoin);
+        hitBox(initX, initY, golden, goldenView,soundCoin, coinStatus1);
+        hitBox(initX, initY, golden2, goldenView2, soundCoin, coinStatus2);
+        hitBoxSmall(initX, initY, golden3, goldenView3, soundCoin, coinStatus3);
+        if (coinsColected >= 3) {
+
+            hitBox(initX, initY, golden4, goldenView4, soundCoin, coinStatus4);
+            hitBoxSmall(initX, initY, golden5, goldenView5, soundCoin, coinStatus5);
+            hitBox(initX, initY, golden6, goldenView6, soundCoin, coinStatus6);
+
+        }
+
 
         circle.setPosition({ initX-5, initY-5 });
 
@@ -200,8 +313,31 @@ int main()
         window.draw(circleRef);
         window.draw(circleRef2);
         window.draw(goldenView);
+        window.draw(goldenView2);
+        window.draw(goldenView3);
+        window.draw(wasdInstructions);
+        window.draw(resetInstr);
+        numOfCoins.setString("Coins collected " + std::to_string(coinsColected));
+        window.draw(numOfCoins);
+        if (coinsColected >= 3) {
+            goldenView.setOutlineColor(sf::Color::Black);
+            goldenView2.setOutlineColor(sf::Color::Black);
+            goldenView3.setOutlineColor(sf::Color::Black);
+            window.draw(goldenView4);
+            window.draw(goldenView5);
+            window.draw(goldenView6);
+        }
+        if (coinsColected >= 6) {
+            window.clear(sf::Color::Black);
+            window.draw(WinMessage);
+            window.display();
+            sf::sleep(sf::seconds(6));
+            break;
+            
+        }
         window.display();
-        Sleep(10);
+        sf::sleep(sf::milliseconds(10));
+
     }
 
     return 0;
